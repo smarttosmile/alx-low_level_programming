6113f4b14a589cff8e38ab029051d45479059a19
root <root@DESKTOP-MTAK92Q>
2024-01-08 20:52:34 +0100

    hello c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -7,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)//int: This specifies the return type of the function. In this case, the function returns an integer
+int _putchar(char c)
 {
-	return (write(1, &c, 1));//return write(1, &c, 1);: This line contains the code that actually performs the main action of the function. It uses the write system call to write the character c to the standard output (1 represents stdout). The function returns the value returned by the write system call, which is the number of bytes written (or an error code if there was an issue)
+	return (write(1, &c, 1));
 }
