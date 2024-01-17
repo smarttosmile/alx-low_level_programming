fa931f3808f0f61a0bf95b8b321c26ac65c247d4
root <root@DESKTOP-MTAK92Q>
2024-01-17 21:53:25 +0100

    hello c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include "main.h"
 
-
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
